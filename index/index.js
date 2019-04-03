de3062ff114105ae1cbb7348fca8edd361a06ac2
founder-ink <founder.ink@outlook.com>
2019-04-04 02:09:13 +0800

    主页样式更新

--- index/index.js
+++ index/index.js
@@ -10,6 +10,79 @@ Page({
       'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
       'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
     ],
+    liquid: [
+      {
+        type: "时光轴",
+        title: "恋爱纪念&&结婚纪念",
+        emoji: "emoji",
+        time: "4月10日",
+        author: "方正",
+        wallpaper: "http://pp8qnwtjv.bkt.clouddn.com/surge-bg3.jpg",
+        FYI: "蛮横天真可爱 都是你不可解的魔术成份"
+      },
+      {
+        type: "时光轴",
+        title: "恋爱纪念&&结婚纪念",
+        emoji: "emoji",
+        time: "4月10日",
+        author: "方正",
+        wallpaper: "http://pp8qnwtjv.bkt.clouddn.com/surge-bg3.jpg",
+        FYI: "蛮横天真可爱 都是你不可解的魔术成份"
+      },
+      {
+        type: "时光轴",
+        title: "恋爱纪念&&结婚纪念",
+        emoji: "emoji",
+        time: "4月10日",
+        author: "方正",
+        wallpaper: "http://pp8qnwtjv.bkt.clouddn.com/surge-bg3.jpg",
+        FYI: "蛮横天真可爱 都是你不可解的魔术成份"
+      },
+      {
+        type: "时光轴",
+        title: "恋爱纪念&&结婚纪念",
+        emoji: "emoji",
+        time: "4月10日",
+        author: "方正",
+        wallpaper: "http://pp8qnwtjv.bkt.clouddn.com/surge-bg3.jpg",
+        FYI: "蛮横天真可爱 都是你不可解的魔术成份"
+      },
+      {
+        type: "时光轴",
+        title: "恋爱纪念&&结婚纪念",
+        emoji: "emoji",
+        time: "4月10日",
+        author: "方正",
+        wallpaper: "http://pp8qnwtjv.bkt.clouddn.com/surge-bg3.jpg",
+        FYI: "蛮横天真可爱 都是你不可解的魔术成份"
+      }
+    ],
+    gift: [
+      {
+        userMessage: "祝福你们",
+        userName: "方正",
+        userImg: "http://pp8qnwtjv.bkt.clouddn.com/surge-bg7.jpg",
+        wallpaper: "http://pp8qnwtjv.bkt.clouddn.com/surge-bg3.jpg"
+      },
+      {
+        userMessage: "祝福你们",
+        userName: "方正",
+        userImg: "http://pp8qnwtjv.bkt.clouddn.com/surge-bg7.jpg",
+        wallpaper: "http://pp8qnwtjv.bkt.clouddn.com/surge-bg3.jpg"
+      },
+      {
+        userMessage: "祝福你们",
+        userName: "方正",
+        userImg: "http://pp8qnwtjv.bkt.clouddn.com/surge-bg4.jpg",
+        wallpaper: "http://pp8qnwtjv.bkt.clouddn.com/surge-bg3.jpg"
+      },
+      {
+        userMessage: "祝福你们",
+        userName: "方正",
+        userImg: "http://pp8qnwtjv.bkt.clouddn.com/surge-bg4.jpg",
+        wallpaper: "http://pp8qnwtjv.bkt.clouddn.com/surge-bg3.jpg"
+      }
+    ]
   },
   redirectToCalendar: function () {
     wx.redirectTo({
